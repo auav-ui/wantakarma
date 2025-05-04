2b3f9f60166c104eec8593b0baf7c8f6e72d64d0
kimnzh <hakimnizami15@gmail.com>
2025-05-05 02:26:34 +0700

    fix: navbar position fixed

--- components/elements/Navbar/Navbar.tsx
+++ components/elements/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import NavbarMobile from "./components/NavbarMobile";
 
 export const Navbar = () => {
   return (
-    <nav className="h-12 lg:h-16 bg-accents-grey-4 py-3 px-6 lg:px-10 font-tt-squares flex justify-between items-center">
+    <nav className="fixed inset-0 h-12 lg:h-16 bg-accents-grey-4 py-3 px-6 lg:px-10 font-tt-squares flex justify-between items-center">
       <img src="/logo.webp" alt="logo" className="h-full" />
       <NavbarDesktop />
       <NavbarMobile />
